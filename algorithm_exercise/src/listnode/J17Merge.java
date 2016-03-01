4921d9efb8a411e0903a56e1b49c91d117b02f13
xiaoluyan <656847263@qq.com>
2016-03-01 22:52:48 +0800

    update j17

--- algorithm_exercise/src/listnode/J17Merge.java
+++ algorithm_exercise/src/listnode/J17Merge.java
@@ -1,5 +1,11 @@
 package listnode;
 
+/**
+ * 题目：输入两个递增排序的链表，
+ * 		合并这两个链表并使新链表中的结点仍然是按照递增排序的。
+ * @author Luyan Xiao
+ *
+ */
 public class J17Merge {
 	
     public ListNode merge(ListNode list1,ListNode list2) {
@@ -9,7 +15,6 @@ public class J17Merge {
         if(list2 == null) {
         	return list1;
         }
-        
         ListNode mergeHead = null;
         if(list1.val < list2.val) {
         	mergeHead = list1;
